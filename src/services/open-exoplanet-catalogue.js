b9ccaa0de6314cc3387c8bc172bf10c4561d3f1a
Peter J. Hart <peter@peterjhart.com>
2019-12-18 03:11:14 -0700

    #3: Added two functions to glean information from the catalog. * All planets that do not have a host star. These have TypeFlag=3. * All planets that orbit a star with the hottest temperature in Kelvins (could be multiple stars if there is a tie).

--- src/services/open-exoplanet-catalogue.js
+++ src/services/open-exoplanet-catalogue.js
@@ -1,8 +1,47 @@
+import filter from "lodash/filter";
+import maxBy from "lodash/maxBy";
 import http from "../utils/http";
 
 const URL =
   "https://gist.githubusercontent.com/joelbirchler/66cf8045fcbb6515557347c05d789b4a/raw/9a196385b44d4288431eef74896c0512bad3defe/exoplanets";
 
+/**
+ * @function getExoplanets
+ * @public
+ * @returns {Promise<Array>}
+ */
 export function getExoplanets() {
   return http.get(URL);
+}
+
+/**
+ * @function getOrphanExoplanets
+ * @public
+ * @param {Array} exoplanets
+ * @returns {Array}
+ */
+export function getOrphanExoplanets(exoplanets) {
+  return filter(exoplanets, p => p.TypeFlag === 3);
+}
+
+/**
+ * @function getMaxStarKelvins
+ * @private
+ * @param {Array} exoplanets
+ * @returns {undefined | Number}
+ */
+function getMaxStarKelvins(exoplanets) {
+  const planetOfMaxKStar = maxBy(exoplanets, "HostStarTempK");
+  return planetOfMaxKStar && planetOfMaxKStar.HostStarTempK;
+}
+
+/**
+ * @function getExoplanetsForHottestStar
+ * @public
+ * @param {Array} exoplanets
+ * @returns {Array}
+ */
+export function getExoplanetsForHottestStar(exoplanets) {
+  const maxK = getMaxStarKelvins(exoplanets);
+  return filter(exoplanets, p => p.HostStarTempK === maxK);
 }
--- src/services/open-exoplanet-catalogue.test.js
+++ src/services/open-exoplanet-catalogue.test.js
@@ -1,5 +1,9 @@
 import http from "../utils/http";
-import { getExoplanets } from "./open-exoplanet-catalogue";
+import {
+  getExoplanets,
+  getOrphanExoplanets,
+  getExoplanetsForHottestStar
+} from "./open-exoplanet-catalogue";
 
 jest.mock("../utils/http");
 
@@ -14,6 +18,157 @@ describe("open-exoplanet-catalogue service", () => {
       const result = await getExoplanets();
       expect(http.get).toHaveBeenCalledWith(catalogUrl);
       expect(result).toEqual(expect.arrayContaining([exoplanet]));
+    });
+  });
+
+  describe("interprets exoplanet data", () => {
+    const exoplanets = [
+      {
+        PlanetIdentifier: "PSO J318.5-22",
+        TypeFlag: 3,
+        PlanetaryMassJpt: 6.5,
+        RadiusJpt: 1.53,
+        PeriodDays: "",
+        SemiMajorAxisAU: "",
+        Eccentricity: "",
+        PeriastronDeg: "",
+        LongitudeDeg: "",
+        AscendingNodeDeg: "",
+        InclinationDeg: "",
+        SurfaceTempK: 1160,
+        AgeGyr: "",
+        DiscoveryMethod: "imaging",
+        DiscoveryYear: 2013,
+        LastUpdated: "13/10/03",
+        RightAscension: "21 14 08",
+        Declination: "+22 51 36",
+        DistFromSunParsec: 24.6,
+        HostStarMassSlrMass: "",
+        HostStarRadiusSlrRad: "",
+        HostStarMetallicity: "",
+        HostStarTempK: "",
+        HostStarAgeGyr: ""
+      },
+      {
+        PlanetIdentifier: "HD 40307 b",
+        TypeFlag: 0,
+        PlanetaryMassJpt: 0.01258,
+        RadiusJpt: "",
+        PeriodDays: 4.3123,
+        SemiMajorAxisAU: 0.0468,
+        Eccentricity: 0.2,
+        PeriastronDeg: "",
+        LongitudeDeg: "",
+        AscendingNodeDeg: "",
+        InclinationDeg: "",
+        SurfaceTempK: 804.5,
+        AgeGyr: "",
+        DiscoveryMethod: "RV",
+        DiscoveryYear: 2008,
+        LastUpdated: "12/11/08",
+        RightAscension: "05 54 04",
+        Declination: "-60 01 24",
+        DistFromSunParsec: 12.8,
+        HostStarMassSlrMass: 0.77,
+        HostStarRadiusSlrRad: "",
+        HostStarMetallicity: -0.31,
+        HostStarTempK: 4977,
+        HostStarAgeGyr: ""
+      },
+      {
+        PlanetIdentifier: "CFBDSIR2149",
+        TypeFlag: 3,
+        PlanetaryMassJpt: 5.5,
+        RadiusJpt: "",
+        PeriodDays: "",
+        SemiMajorAxisAU: "",
+        Eccentricity: "",
+        PeriastronDeg: "",
+        LongitudeDeg: "",
+        AscendingNodeDeg: "",
+        InclinationDeg: "",
+        SurfaceTempK: "",
+        AgeGyr: "",
+        DiscoveryMethod: "imaging",
+        DiscoveryYear: 2012,
+        LastUpdated: "12/11/14",
+        RightAscension: "21 49 47",
+        Declination: "-04 03 08",
+        DistFromSunParsec: 40,
+        HostStarMassSlrMass: "",
+        HostStarRadiusSlrRad: "",
+        HostStarMetallicity: "",
+        HostStarTempK: "",
+        HostStarAgeGyr: ""
+      },
+      {
+        PlanetIdentifier: "Kepler-291 b",
+        TypeFlag: 0,
+        PlanetaryMassJpt: "",
+        RadiusJpt: 0.19684,
+        PeriodDays: 3.546511,
+        SemiMajorAxisAU: "",
+        Eccentricity: "",
+        PeriastronDeg: "",
+        LongitudeDeg: "",
+        AscendingNodeDeg: "",
+        InclinationDeg: "",
+        SurfaceTempK: "",
+        AgeGyr: "",
+        DiscoveryMethod: "transit",
+        DiscoveryYear: 2014,
+        LastUpdated: "14/02/26",
+        RightAscension: "19 11 39",
+        Declination: "+42 26 14",
+        DistFromSunParsec: 1899.9,
+        HostStarMassSlrMass: 1.01,
+        HostStarRadiusSlrRad: 1.016,
+        HostStarMetallicity: "",
+        HostStarTempK: 6002,
+        HostStarAgeGyr: ""
+      },
+      {
+        PlanetIdentifier: "Kepler-291 c",
+        TypeFlag: 0,
+        PlanetaryMassJpt: "",
+        RadiusJpt: 0.17132,
+        PeriodDays: 5.700786,
+        SemiMajorAxisAU: "",
+        Eccentricity: "",
+        PeriastronDeg: "",
+        LongitudeDeg: "",
+        AscendingNodeDeg: "",
+        InclinationDeg: "",
+        SurfaceTempK: "",
+        AgeGyr: "",
+        DiscoveryMethod: "transit",
+        DiscoveryYear: 2014,
+        LastUpdated: "14/02/26",
+        RightAscension: "19 11 39",
+        Declination: "+42 26 14",
+        DistFromSunParsec: 1899.9,
+        HostStarMassSlrMass: 1.01,
+        HostStarRadiusSlrRad: 1.016,
+        HostStarMetallicity: "",
+        HostStarTempK: 6002,
+        HostStarAgeGyr: ""
+      }
+    ];
+
+    it("should get the planets that do not orbit a star", () => {
+      const result = getOrphanExoplanets(exoplanets);
+      expect(result.length).toBe(2);
+      const ids = result.map(r => r.PlanetIdentifier);
+      const expectedIds = ["CFBDSIR2149", "PSO J318.5-22"];
+      expect(ids).toEqual(expect.arrayContaining(expectedIds));
+    });
+
+    it("should get the planets that orbit the hottest star", () => {
+      const result = getExoplanetsForHottestStar(exoplanets);
+      expect(result.length).toBe(2);
+      const ids = result.map(r => r.PlanetIdentifier);
+      const expectedIds = ["Kepler-291 b", "Kepler-291 c"];
+      expect(ids).toEqual(expect.arrayContaining(expectedIds));
     });
   });
 });
